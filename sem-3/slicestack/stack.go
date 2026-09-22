f3cbaee802e98865665ed091a540942c58ba6101
Fariza <faarshamileva@edu.hse.ru>
2026-09-22 23:58:03 +0300

    sem-3: slicestack

--- sem-3/slicestack/stack.go
+++ sem-3/slicestack/stack.go
@@ -4,19 +4,23 @@ package slicestack
 
 // Push добавляет value на вершину стека.
 func Push(stack []int, value int) []int {
-	_ = value
-
-	return stack
+	return append(stack, value)
 }
 
 // Peek возвращает верхнее значение, не меняя стек.
 func Peek(stack []int) (value int, err error) {
-	_ = stack
+	if len(stack) == 0 {
+		return 0, ErrEmpty
+	}
 
-	return 0, nil
+	return stack[len(stack)-1], nil
 }
 
 // Pop возвращает верхнее значение и оставшийся стек.
 func Pop(stack []int) (value int, rest []int, err error) {
-	return 0, stack, nil
+	if len(stack) == 0 {
+		return 0, stack, ErrEmpty
+	}
+
+	return stack[len(stack)-1], stack[:len(stack)-1], nil
 }
