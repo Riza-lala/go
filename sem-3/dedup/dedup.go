0316f330fcbd01efa45be7bc74865a2fca964aee
Fariza <faarshamileva@edu.hse.ru>
2026-09-22 20:48:08 +0300

    sem3 dedup zadacha

--- sem-3/dedup/dedup.go
+++ sem-3/dedup/dedup.go
@@ -4,7 +4,18 @@ package dedup
 
 // Deduplicate удаляет повторы, сохраняя порядок первых появлений.
 func Deduplicate(input []string) []string {
-	_ = input
+	if input == nil {
+		return nil
+	}
+	seen := make(map[string]bool)
+	result := []string{}
+	for _, s := range input {
+		if seen[s] {
+			continue
+		}
+		seen[s] = true
+		result = append(result, s)
+	}
 
-	return nil
+	return result
 }
